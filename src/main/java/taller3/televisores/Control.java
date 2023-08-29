cc7422afe38956b2e2bfc033ba340e3909bc493f
= <60821161+Agua-bit@users.noreply.github.com>
2023-08-29 04:11:39 -0500

    aaa

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,4 +1,4 @@
-
+package taller3.televisores
 
 public class Control{
 
--- src/main/java/taller3/televisores/Marca.java
+++ src/main/java/taller3/televisores/Marca.java
@@ -1,3 +1,5 @@
+package taller3.televisores
+
 public class Marca{
     
     private String nombre;
@@ -5,7 +7,7 @@ public class Marca{
     public Marca (String nombre){
         this.nombre = nombre;
     }
-    
+
     public String getNombre(){
         return nombre;
     }
